ffa824bdc6f56fca952c336f0872acc761a6479e
velame <mvp2@cesar.school>
2025-04-10 22:38:09 -0300

    update

---
+++ bdfabrica/src/main/java/com/fabricaagricola/bdfabrica/BdfabricaApplication.java
@@ -0,0 +1,13 @@
+package com.fabricaagricola.bdfabrica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BdfabricaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BdfabricaApplication.class, args);
+	}
+
+}
---
+++ bdfabrica/src/test/java/com/fabricaagricola/bdfabrica/BdfabricaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fabricaagricola.bdfabrica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BdfabricaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
