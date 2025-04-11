93aa24a8f63757e59a4511690ea61e09108bff4a
velame <mvp2@cesar.school>
2025-04-11 16:14:05 -0300

    alteraçoes

--- bdfabrica/src/main/java/com/fabricaagricola/bdfabrica/BdfabricaApplication.java
+++ bdfabrica/src/main/java/com/fabricaagricola/bdfabrica/BdfabricaApplication.java
@@ -1,13 +1,82 @@
 package com.fabricaagricola.bdfabrica;
+import jakarta.persistence.*;
+import java.util.Date;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import com.fabricaagricola.bdfabrica.model.Financeiro;
 
-@SpringBootApplication
+@Entity
+@Table(name = "Contas")
 public class BdfabricaApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BdfabricaApplication.class, args);
-	}
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "IdConta")
+    private Integer idConta;
 
+    @ManyToOne
+    @JoinColumn(name = "IdFinanceiro", referencedColumnName = "IdFinanceiro")
+    private Financeiro financeiro;
+
+    @Column(name = "dataEmissao")
+    @Temporal(TemporalType.DATE)
+    private Date dataEmissao;
+
+    @Column(name = "dataVencimento")
+    @Temporal(TemporalType.DATE)
+    private Date dataVencimento;
+
+    @Column(name = "valorTotal")
+    private Float valorTotal;
+
+    @Column(name = "status")
+    private String status;
+
+    // Getters e Setters
+    public Integer getIdConta() {
+        return idConta;
+    }
+
+    public void setIdConta(Integer idConta) {
+        this.idConta = idConta;
+    }
+
+    public Financeiro getFinanceiro() {
+        return financeiro;
+    }
+
+    public void setFinanceiro(Financeiro financeiro) {
+        this.financeiro = financeiro;
+    }
+
+    public Date getDataEmissao() {
+        return dataEmissao;
+    }
+
+    public void setDataEmissao(Date dataEmissao) {
+        this.dataEmissao = dataEmissao;
+    }
+
+    public Date getDataVencimento() {
+        return dataVencimento;
+    }
+
+    public void setDataVencimento(Date dataVencimento) {
+        this.dataVencimento = dataVencimento;
+    }
+
+    public Float getValorTotal() {
+        return valorTotal;
+    }
+
+    public void setValorTotal(Float valorTotal) {
+        this.valorTotal = valorTotal;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
 }
