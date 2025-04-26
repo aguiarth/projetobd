6d508ddceab470d015b6d15a170b866f0147fb45
Maria Júlia Peixoto <143026488+majupeixoto@users.noreply.github.com>
2025-04-26 14:10:48 -0300

    Update Projeto_bd.sql

--- Script/Projeto_bd.sql
+++ Script/Projeto_bd.sql
@@ -49,14 +49,15 @@ CREATE TABLE Possui (
 );
 
 CREATE TABLE NotaFiscal (
-    chave VARCHAR(45) PRIMARY KEY,
+    chave INT AUTO_INCREMENT PRIMARY KEY,
     valor_imposto FLOAT CHECK (valor_imposto >= 0),
     data_emissao_nota DATE DEFAULT (CURRENT_DATE)
 );
 
+
 CREATE TABLE Pedido (
     numero VARCHAR(45) PRIMARY KEY,
-    chave VARCHAR(45),
+    chave INT,
     data_emissao DATE DEFAULT (CURRENT_DATE),
     valor_total FLOAT CHECK (valor_total >= 0),
     status VARCHAR(45) CHECK (status IN ('ABERTO', 'FINALIZADO', 'CANCELADO')),
