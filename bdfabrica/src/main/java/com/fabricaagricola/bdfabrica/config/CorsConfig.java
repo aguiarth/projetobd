6591606e6fedc5bdc800e87554a83757f7fc6955
velame <mvp2@cesar.school>
2025-04-29 22:17:59 -0300

    ajustes

--- bdfabrica/src/main/java/com/fabricaagricola/bdfabrica/config/CorsConfig.java
+++ bdfabrica/src/main/java/com/fabricaagricola/bdfabrica/config/CorsConfig.java
@@ -7,16 +7,15 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfig {
-
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000") // Porta do React
-                        .allowedMethods("GET", "POST", "PUT", "DELETE")
-                        .allowedHeaders("*");
+                    .allowedOrigins("http://localhost:3000")
+                    .allowedMethods("GET", "POST", "PUT", "DELETE")
+                	.allowedHeaders("*");
             }
         };
     }
