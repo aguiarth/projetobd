129aa0d44f2df3e2cc56d70f504b74dbd7eb1949
Maju <mjpo@cesar.school>
2025-05-24 17:36:14 -0300

    .

--- bdfabrica/src/main/java/com/fabricaagricola/bdfabrica/service/ExpedicaoService.java
+++ bdfabrica/src/main/java/com/fabricaagricola/bdfabrica/service/ExpedicaoService.java
@@ -5,7 +5,6 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import com.fabricaagricola.bdfabrica.model.Expedicao;
 import com.fabricaagricola.bdfabrica.repository.EntregaTransporteRepository;
